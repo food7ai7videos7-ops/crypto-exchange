799f0a1bbfb6475803415c71bffe40588ec90e6d
food7ai7videos7-ops <food7ai7videos7@gmail.com>
2026-09-05 23:46:18 +0500

    Update server.js

--- server.js
+++ server.js
@@ -161,6 +161,4 @@ app.post('/api/trade/close', async (req, res) => {
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-});app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
 });
